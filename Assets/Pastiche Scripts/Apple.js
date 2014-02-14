97c6596dca11da7397c2aa26640271650675455b
Eric Lam <elam3@ualberta.ca>
2014-02-14 01:52:45 -0700

    second commit

---
+++ Assets/Pastiche Scripts/Apple.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var speed : float = 6.0;
+var maxDistance : float = 25.0;
+
+function Start () {
+	maxDistance += transform.position.x;
+}
+
+function Update () {
+
+	var xMove = Time.deltaTime * speed;
+
+	transform.Translate(xMove, 0, 0);
+
+	var viewPoint = Camera.main.WorldToViewportPoint(transform.position);
+	viewPoint.x = Mathf.Clamp01(viewPoint.x);
+	viewPoint.y = Mathf.Clamp01(viewPoint.y);
+
+	if(transform.position.x > maxDistance) {
+		Destroy(gameObject);
+	}
+
+}
+
+function OnCollisionEnter2D(coll : Collision2D) {
+	if(coll.gameObject.tag == "Enemy") {
+		Debug.Log("BOO");
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Pastiche Scripts/MiniMona.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+var speed : float = 4.0;
+var maxDistance : float = 25.0;
+
+function Start () {
+	maxDistance = transform.position.x - maxDistance;
+}
+
+function Update () {
+
+	var xMove = Time.deltaTime * -speed;
+
+	transform.Translate(xMove, 0, 0);
+
+	var viewPoint = Camera.main.WorldToViewportPoint(transform.position);
+	viewPoint.x = Mathf.Clamp01(viewPoint.x);
+	viewPoint.y = Mathf.Clamp01(viewPoint.y);
+
+	if(transform.position.x < maxDistance) {
+		Destroy(gameObject);
+	}
+}
+
+function OnCollisionEnter2D(coll : Collision2D) {
+	Debug.Log("HIT");
+	if(coll.gameObject.tag == "Apple") {
+		transform.GetChild(0).gameObject.SetActive(false);
+		animation.Play();
+		yield WaitForSeconds(1.5);
+		Destroy(gameObject);
+	}
+}
